41683faedafcf425e291ae372e9607c315cad831
joschareuther <92160393+joschareuther@users.noreply.github.com>
2021-11-25 00:50:05 +0100

    Aufgabe07 mit Spezialsounds

--- Aufgabe07/Script07.js
+++ Aufgabe07/Script07.js
@@ -1,14 +1,20 @@
-function play() {
-    var audio = new Audio('audios/Audio1.mp3');
-    audio.play();
+var samp = [new Audio('audios/Audio4.mp3'), new Audio('audios/Audio3.mp3'), new Audio('audios/Audio2.mp3'),
+    new Audio('audios/Audio6.mp3'), new Audio('assets/hihat.mp3'), new Audio('assets/kick.mp3'),
+    new Audio('assets/snare.mp3'), new Audio('audios/Audio7.mp3'), new Audio('audios/Audio8.mp3'),
+    new Audio('assets/.mp3')];
+document.querySelector("#button1").addEventListener("click", function () { playSample(samp[0], "", ""); });
+document.querySelector("#button2").addEventListener("click", function () { playSample(samp[1], "", ""); });
+document.querySelector("#button3").addEventListener("click", function () { playSample(samp[2], "", ""); });
+document.querySelector("#button4").addEventListener("click", function () { playSample(samp[3], "", ""); });
+document.querySelector("#button5").addEventListener("click", function () { playSample(samp[4], "", ""); });
+document.querySelector("#button6").addEventListener("click", function () { playSample(samp[5], "", ""); });
+document.querySelector("#button7").addEventListener("click", function () { playSample(samp[6], "", ""); });
+document.querySelector("#button8").addEventListener("click", function () { playSample(samp[7], "", ""); });
+document.querySelector("#button9").addEventListener("click", function () { playSample(samp[8], "", ""); });
+document.querySelector("#playbutton").addEventListener("click", function () { playSample(samp[4], samp[5], samp[6]); });
+function playSample(ton, snare, hihat) {
+    setInterval(function () { ton.play(); }, 0);
+    setInterval(function () { snare.play(); }, 1000);
+    setInterval(function () { hihat.play(); }, 2000);
 }
-document.querySelector("#button1").addEventListener("click", play);
-document.querySelector("#button2").addEventListener("click", play);
-document.querySelector("#button3").addEventListener("click", play);
-document.querySelector("#button4").addEventListener("click", play);
-document.querySelector("#button5").addEventListener("click", play);
-document.querySelector("#button6").addEventListener("click", play);
-document.querySelector("#button7").addEventListener("click", play);
-document.querySelector("#button8").addEventListener("click", play);
-document.querySelector("#button9").addEventListener("click", play);
 //# sourceMappingURL=Script07.js.map
