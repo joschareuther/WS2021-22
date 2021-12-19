0129c54efd69925502a185ffd89cbc437400046e
joschareuther <92160393+joschareuther@users.noreply.github.com>
2021-12-19 23:25:12 +0100

    Update aufgabe9

--- Aufgabe09/script09.js
+++ Aufgabe09/script09.js
@@ -4,20 +4,27 @@ window.addEventListener("load", function () {
     var Todo;
     ((_Todo) => {
         // tslint:disable-next-line: typedef
-        var app = document.getElementById("app");
+        let app = document.getElementById("app");
         // tslint:disable-next-line: typedef
-        var p = document.createElement("p"); // <p></p>
+        let p = document.createElement("p"); // <p></p>
         p.textContent = "Erstelle hier deine ToDo-Liste:";
         app.appendChild(p);
         document.querySelector("#addButton").addEventListener("click", addTextToHtml);
         // tslint:disable-next-line: typedef
-        var input = document.querySelector("#inputTodo");
+        var input2 = document.getElementById("inputTodo");
+        input2.addEventListener("keydown", function (e) {
+            if (e.keyCode === 13) { //checks whether the pressed key is "Enter"
+                addTextToHtml();
+            }
+        });
+        // tslint:disable-next-line: typedef
+        let input = document.querySelector("#inputTodo");
         // tslint:disable-next-line: typedef
         function addTextToHtml() {
             var textVonEingabefeld;
-            if (input.value != "")
-                textVonEingabefeld = input.value;
-            input.value = "";
+            if (input2.value != "")
+                textVonEingabefeld = input2.value;
+            input2.value = "";
             // tslint:disable-next-line: typedef
             var app = document.getElementById("app");
             // tslint:disable-next-line: typedef
